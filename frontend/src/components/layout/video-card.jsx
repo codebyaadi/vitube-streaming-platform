2cb3cf1a9ea10a4f65564944d0084f9ffdf6d539
uixaadi <uixaadi@gmail.com>
2023-10-10 23:32:49 +0530

    Modify Video Card

--- frontend/src/components/layout/video-card.jsx
+++ frontend/src/components/layout/video-card.jsx
@@ -1,21 +1,41 @@
-import React from 'react'
-import ReactPlayer from "react-player"
-import { Link } from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 
-const VideoCard = ({title, description, like, comment, videoUrl}) => {
+const VideoCard = ({
+  title,
+  views,
+  thumbnailUrl,
+  profilePicture,
+  uploadedAgo,
+  videoId
+}) => {
   return (
     <div className="bg-[#282828] border border-[#3E3E3E] w-64 p-2">
-        <div id="video-container"className="w-full h-64">
-            <ReactPlayer width={"100%"} height={"100%"} url={videoUrl} controls />   
+      <div id="video-container" className="w-full h-32 overflow-hidden">
+        <img className="w-full object-cover" src={thumbnailUrl} alt={title} />
+      </div>
+      <div className="w-full flex flex-col gap-2 justify-start items-start mt-2 font-prompt">
+        <div className="flex gap-2 justify-start">
+          <img
+            className="w-10 h-10 rounded-full object-cover"
+            src={profilePicture}
+            alt={profilePicture}
+          />
+          <Link to={`/video/${videoId}`} className="text-base font-medium line-clamp-2">
+            {title}
+          </Link>
         </div>
-        <div className="flex flex-col gap-2 justify-start items-start mt-2 font-prompt">
-            <Link to="/">{title}</Link>
-            <h3>{description}</h3>
-            <span>Like: {like}</span>
-            <span>Comment: {comment}</span>
+        <div className="w-full flex justify-between">
+          <span className="font-opensans font-semibold text-xs text-gray-400">
+            Views: {views}
+          </span>
+          <span className="font-opensans font-semibold text-xs text-gray-400">
+            {uploadedAgo}
+          </span>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default VideoCard
+export default VideoCard;
