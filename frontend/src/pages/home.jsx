d36fac26b1a5bd722ac453ae4146ae466a8a061b
uixaadi <uixaadi@gmail.com>
2023-10-10 23:33:53 +0530

    Replaced Video with Thumbnail

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,15 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import useUserInfoFromToken from '../hooks/userinfo';
-import VideoCard from '../components/layout/video-card';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import useUserInfoFromToken from "../hooks/userinfo";
+import VideoCard from "../components/layout/video-card";
+import axios from "axios";
 
 const Home = () => {
   const [videos, setVideos] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:8080/api/v1/getAllVideos')
+    axios
+      .get("http://localhost:8080/api/v1/getAllVideos")
       .then((response) => {
         setVideos(response.data);
+        console.log(response.data)
       })
       .catch((error) => {
         console.error(error);
@@ -19,7 +21,16 @@ const Home = () => {
   return (
     <div className="mt-16 flex gap-2 flex-wrap">
       {videos.map((video) => (
-        <VideoCard key={video._id} title={video.title} videoUrl={video.videoUrl} like={video.likes} comment={video.comment} />
+        <VideoCard
+          key={video._id}
+          videoId={video._id}
+          title={video.title}
+          thumbnailUrl={video.thumbnailUrl}
+          views={video.views}
+          uploadedAgo={video.uploadedAgo}
+          comment={video.comment}
+          profilePicture={video.uploadedBy.profilePicture}
+        />
       ))}
     </div>
   );
