0511fdc25e22688c9b6899e761b1a13401be9e6f
uixaadi <uixaadi@gmail.com>
2023-10-12 22:05:36 +0530

    Added controller for updating  profile image and controller for verify OTP

--- backend/db/controllers/userControllers.js
+++ backend/db/controllers/userControllers.js
@@ -1,10 +1,18 @@
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 import * as dotenv from "dotenv";
+import cloudinary from "cloudinary";
 import User from "../models/Users.js";
+import nodeMailer from "../utils/nodeMail.js";
 
 dotenv.config();
 const secretKey = process.env.JWT_SECRET_KEY;
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
 
 // // // // // // // // // // // // // // // // // // // // // // // // // //
 // *                         SIGN UP CONTROLLER                          * //
@@ -38,6 +46,7 @@ export const createUser = async (req, res) => {
             password: hashedPassword,
         });
 
+        await nodeMailer({userEmail: email})
         return res.status(201).json({ success: true, data: newUser });
     } catch (error) {
         console.error("Error in user sign-up:", error);
@@ -102,6 +111,21 @@ export const logIn = async (req, res) => {
 export const updateUser = async (req, res) => {
     try {
         const { userId, username, name } = req.body;
+        const image = req.file;
+        console.log(image);
+        if (!image) {
+            return res.status(400).json({ message: 'Image file is required.' });
+        }
+
+        // Convert the file buffer to a data URL //
+        const dataUrl = `data:${image.mimetype};base64,${image.buffer.toString('base64')}`;
+
+        // Upload data URL to Cloudinary and get the image URL
+        const uploadedImage = await cloudinary.v2.uploader.upload(dataUrl);
+
+        if (!uploadedImage.secure_url) {
+            return res.status(500).json({ message: 'Image upload failed.' });
+        }
 
         const user = await User.findById(userId);
 
@@ -117,8 +141,42 @@ export const updateUser = async (req, res) => {
             user.fullName = name;
         }
 
+        user.profilePicture = uploadedImage.secure_url;
+
         await user.save();
-        res.status(200).json({ message: "Changes updated successfull" });
+        return res.status(200).json({ message: "Changes updated successfull", data: user });
+    } catch (error) {
+        console.error("Error while updating profile:", error);
+        return res.status(500).json({ message: "Something went wrong." });
+    }
+};
+
+// // // // // // // // // // // // // // // // // // // // // // // // // //
+// *                       VERIFY OTP CONTROLLER                         * //
+// // // // // // // // // // // // // // // // // // // // // // // // // //
+
+export const verifyOtp = async (req, res) => {
+    try {
+        const { email, enteredOtp } = req.body;
+        console.log(email);
+        console.log(enteredOtp);
+
+        const user = await User.findOne({ email });
+
+        if (!user) { // Check if user is not found, not email
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        const otp = user.emailOTP;
+        console.log(otp)
+
+        if (enteredOtp == otp) { // Use strict equality (===) for comparison
+            user.isEmailVerified = true;
+            await user.save(); // Save the user with the updated email verification status
+            return res.status(200).json({ message: "Email is verified." });
+        } else {
+            return res.status(401).json({ message: "Incorrect OTP" });
+        }
     } catch (error) {
         console.error("Error while updating profile:", error);
         return res.status(500).json({ message: "Something went wrong." });
