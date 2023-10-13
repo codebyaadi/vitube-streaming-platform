e9f96872ae2e1e51c1dc392812328ccb43a1d83d
uixaadi <uixaadi@gmail.com>
2023-10-13 20:00:16 +0530

    Changes made to api

--- frontend/src/api/base/config.js
+++ frontend/src/api/base/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080/api/v1",
+    baseURL: "https://vitube-streaming-platform-backend.vercel.app/api/v1",
 })
 
 export default api;
