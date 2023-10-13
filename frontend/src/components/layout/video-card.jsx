500a4b157839ec70eafdc0d786f66d1044122bd5
uixaadi <uixaadi@gmail.com>
2023-10-13 18:52:10 +0530

    Some minor changes for responsiveness in video-card.jsx

--- frontend/src/components/layout/video-card.jsx
+++ frontend/src/components/layout/video-card.jsx
@@ -11,17 +11,17 @@ const VideoCard = ({
 }) => {
   return (
     <div className="bg-[#282828] border border-[#3E3E3E] w-full lg:w-72 md:w-80 p-2">
-      <div id="video-container" className="w-full h-32 overflow-hidden">
+      <div id="image-container" className="w-full h-32 overflow-hidden">
         <img className="w-full object-cover" src={thumbnailUrl} alt={title} />
       </div>
       <div className="w-full flex flex-col gap-2 justify-start items-start mt-2 font-prompt">
-        <div className="flex gap-2 justify-start">
+        <div className="flex flex-row flex-auto gap-2 justify-between items-center">
           <img
-            className="w-10 h-10 rounded-full object-cover"
+            className="flex-shrink-0 w-10 h-10 rounded-full object-cover"
             src={profilePicture}
             alt={profilePicture}
           />
-          <Link to={`/video/${videoId}`} className="text-base font-medium line-clamp-2">
+          <Link to={`/video/${videoId}`} className="flex-1 text-base font-medium line-clamp-2">
             {title}
           </Link>
         </div>
