c9382ed937150809124d59bb176ec7fc63c6e4b7
uixaadi <uixaadi@gmail.com>
2023-10-14 19:16:34 +0530

    Allowed Cross-Origin site

--- backend/index.js
+++ backend/index.js
@@ -2,17 +2,27 @@ import * as dotenv from "dotenv";
 import express from "express";
 import connectDB from "./db/config/connect.js";
 import cors from "cors";
-import userRoutes from "./db/routes/userRoutes.js"
-import videoRoutes from "./db//routes/videoRoutes.js"
+import userRoutes from "./db/routes/userRoutes.js";
+import videoRoutes from "./db/routes/videoRoutes.js";
 
 dotenv.config();
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: [
+    'https://vitube-streaming-platform.vercel.app',
+    'http://localhost:5173',
+  ],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204, // Some legacy browsers choke on 204
+}));
+
 app.use(express.json());
 
 app.use("/api", userRoutes);
-app.use("/api", videoRoutes)
+app.use("/api", videoRoutes);
 
 app.get('/', (req, res) => {
   res.status(200).json({
@@ -33,7 +43,7 @@ const startServer = async () => {
     await connectDB(process.env.MONGO_URI);
     app.listen(8080, () => console.log("Server started on port 8080"));
   } catch (error) {
-    console.error("Error connecting to the database:" , error);
+    console.error("Error connecting to the database:", error);
     process.exit(1);
   }
 };
