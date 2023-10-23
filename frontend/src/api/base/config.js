b51ccd371e581e3d9be34ccb49d735ede0ce6de8
uixaadi <uixaadi@gmail.com>
2023-10-23 23:45:38 +0530

    Set baseURL

--- frontend/src/api/base/config.js
+++ frontend/src/api/base/config.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
     // Uncomment below line for Local Development
-    baseURL: "http://localhost:8080/api/v1",
-    // baseURL: import.meta.env.VITE_API_BASE_URL,
+    // baseURL: "http://localhost:8080/api/v1",
+    baseURL: import.meta.env.VITE_API_BASE_URL,
 })
 
 export default api;
