a99e5dea254bb5b9823d5c118e7fc25a57f680f1
adityastar57 <adityastar57@gmail.com>
2024-08-16 07:31:35 +0000

    feat: prisma setup and endpoint for adding user completed

--- backend-elysia/src/handlers/users.handler.ts
+++ backend-elysia/src/handlers/users.handler.ts
@@ -1,41 +1,89 @@
-import prisma from "@/lib/db";
 import Elysia, { t } from "elysia";
+import cookie from "@elysiajs/cookie";
+import prisma from "@/lib/db";
+import { getRandomProfileImg } from "@/lib/utils";
 
-export const users = new Elysia().group("/user", (app) =>
-    app.post(
-        "/signup",
-        async ({ body, set }) => {
-            try {
-                const { fullname, username, email, password } = body;
+export const users = new Elysia().use(cookie()).group("/user", (app) =>
+    app
+        .post(
+            "/signup",
+            async ({ body, set }) => {
+                try {
+                    const { fullname, username, email, password } = body;
 
-                const user = await prisma.users.findFirst({
-                    where: {
-                        OR: [
-                            {
-                                email,
-                            },
-                            {
-                                username,
-                            },
-                        ],
-                    },
-                });
+                    const user = await prisma.user.findFirst({
+                        where: {
+                            OR: [
+                                {
+                                    email,
+                                },
+                                {
+                                    username,
+                                },
+                            ],
+                        },
+                    });
 
-                if (!fullname || !username || !email || !password) {
-                    set.status = 400;
+                    if (!fullname || !username || !email || !password) {
+                        set.status = 400;
+                        return {
+                            message: "Fill up all the details",
+                        };
+                    }
+
+                    if (user) {
+                        set.status = 400;
+                        return {
+                            message: "User already exist",
+                        };
+                    }
+
+                    const pImg = getRandomProfileImg();
+                    const hashedPasswd = await Bun.password.hash(password);
+
+                    const newUser = await prisma.user.create({
+                        data: {
+                            fullName: fullname,
+                            username,
+                            email,
+                            password: hashedPasswd,
+                            profilePicture: pImg,
+                        },
+                    });
+
+                    set.status = 201;
                     return {
-                        message: "Fill up all the details",
+                        message: "User created successfully",
+                        data: newUser,
+                    };
+                } catch (error) {
+                    set.status = 500;
+                    return {
+                        message: "Something went wrong",
                     };
                 }
-            } catch (error) {}
-        },
-        {
-            body: t.Object({
-                fullname: t.String(),
-                username: t.String(),
-                email: t.String(),
-                password: t.String(),
-            }),
-        }
-    )
+            },
+            {
+                body: t.Object({
+                    fullname: t.String(),
+                    username: t.String(),
+                    email: t.String(),
+                    password: t.String(),
+                }),
+            }
+        )
+        .post(
+            "/signin",
+            async ({ body, set }) => {
+                try {
+                    const { email, password } = body;
+                } catch (error) {}
+            },
+            {
+                body: t.Object({
+                    email: t.String(),
+                    password: t.String(),
+                }),
+            }
+        )
 );
--- backend-elysia/src/index.ts
+++ backend-elysia/src/index.ts
@@ -1,6 +1,10 @@
 import { Elysia } from "elysia";
+import { users } from "./handlers/users.handler";
 
-const app = new Elysia().get("/", () => "Hello Elysia + Vitube").listen(3000);
+const app = new Elysia()
+    .get("/", () => "Hello Elysia + Vitube")
+    .use(users)
+    .listen(3000);
 
 console.log(
     `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
