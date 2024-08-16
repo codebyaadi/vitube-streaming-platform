068950eb00253460d805e01ae69251d8b02a21fc
adityastar57 <adityastar57@gmail.com>
2024-08-16 10:45:36 +0000

    setup: new frontend created with Panda CSS & Park UI

--- www/src/app/page.tsx
+++ www/src/app/page.tsx
@@ -1,9 +1,7 @@
-
-
 export default function Home() {
-  return (
-    <main className="">
-      <h1>Hello</h1>
-    </main>
-  );
+    return (
+        <main className="">
+            <h1>Hello</h1>
+        </main>
+    );
 }
