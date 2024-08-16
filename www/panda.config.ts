0405393016f41ee556a1ac4c302d05f43dc9d69c
adityastar57 <adityastar57@gmail.com>
2024-08-16 10:43:23 +0000

    setup: new frontend created with Panda CSS & Park UI

---
+++ www/panda.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "@pandacss/dev";
+
+export default defineConfig({
+  // Whether to use css reset
+  preflight: true,
+
+  // presets
+  presets: ['@pandacss/preset-base', '@park-ui/panda-preset'],
+
+  // Where to look for your css declarations
+  include: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
+
+  // Files to exclude
+  exclude: [],
+
+  // Useful for theme customization
+  theme: {
+    extend: {},
+  },
+
+  // Framework
+  jsxFramework: 'react',
+
+  // The output directory for your css system
+  outdir: "styled-system",
+});
---
+++ www/src/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <main className="">
+      <h1>Hello</h1>
+    </main>
+  );
+}
